507d3f41d0b00bdb932b0144da49cd1ef9f6520c
unknown <michaelcordero88@gmail.com>
2018-02-12 17:05:50 -0800

    Margin adjustments

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
 var pomodoro={
              img:"./images/Pomodoro.png",
              name:"Pomodoro Timer",
-             notes:"A Pomodoro Timer with task list. Drag and drop is utilized to provide one list for brainstorming and one for whatever your currently working on",
+             notes:"A Pomodoro Timer with task list. Drag and drop is utilized to provide one list for brainstorming and one for whatever your currently working on.",
              link:"https://codepen.io/Cyberputty/full/NXjRgX/",
               };
               
 var ticTacToe={
              img:"./images/Tic-Tac-Toe.png",
              name:"Tic Tac Toe",
-             notes:"The challenge for this was to create the AI logic and a game loop",
+             notes:"The challenge for this was to create the AI logic and a game loop.",
              link: "https://codepen.io/Cyberputty/full/QaPwzo/"};
 var simon={
              img:"./images/Simon.png",
              name:"Simon",
-             notes:"Simon the classic memory game, the visuals are entirely css. Converts to and from string id's, generates random sequence per game",
+             notes:"Simon the classic memory game, the visuals are entirely css. Converts to and from string id's, generates random sequence per game.",
              link:"https://codepen.io/Cyberputty/full/aqdOWy/"};
 var twitch={
              img:"./images/TwitchApp.png",
              name:"Twitch Notifications",
-             notes:"App using Twitch API for notifications",
+             notes:"App using Twitch API for notifications.",
              link:"https://codepen.io/Cyberputty/full/wydGdM/"};
 var $currentProject=$('#currentProject');
 var $projectNote=$('#projectNote');
